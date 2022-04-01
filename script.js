6909c5c9da71f485916ba338ef89896e09c81415
Jéssica <jessicavitorsantos@outlook.com>
2022-04-01 16:52:27 -0300

    três seleçoes, uma em cada linha

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
-function escolherPrato(pratoClicado) {
+function pratoPrincipal (pratoClicado) {
     const elemento = document.querySelector(".borda-verde");
     if (elemento !== null) {
         elemento.classList.toggle("borda-verde");
     }
 
     pratoClicado.classList.add("borda-verde");
+
+  
 }
+
+function bebida (pratoClicado) {
+    const elemento = document.querySelector(".borda");
+    if (elemento !== null) {
+        elemento.classList.toggle("borda");
+    }
+
+    pratoClicado.classList.add("borda");
+
+  
+}
+
+function sobremesa (pratoClicado) {
+    const elemento = document.querySelector(".borda-green");
+    if (elemento !== null) {
+        elemento.classList.toggle("borda-green");
+    }
+
+    pratoClicado.classList.add("borda-green");
+
+  
+}
+
 
