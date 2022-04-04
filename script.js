f23e6537f5b47ba16043e96086b52d5184684d6c
Jéssica <jessicavitorsantos@outlook.com>
2022-04-04 16:51:48 -0300

    final

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 let prato;
 let bebida;
 let sobremesa;
+let prato1;
+let check;
 
 function escolherPrato(pratoClicado) {
+   
     prato = pratoClicado.innerHTML;
 
     const elemento = document.querySelector(".borda-verde");
-    const prato1 = document.querySelector("ion-icon");
 
     if (elemento !== null) {
         elemento.classList.remove("borda-verde");
-        prato1.classList.add("escondido")
+        prato1.classList.add("escondido");
     } else {
-    pratoClicado.classList.add("borda-verde");
-    prato1.classList.remove("escondido") }
+    pratoClicado.classList.add("borda-verde");}
+    prato1 = document.querySelector(".borda-verde").querySelector("ion-icon");
+    prato1.classList.remove("escondido");
+    
 
 }
 
@@ -21,30 +25,31 @@ function escolherBebida(bebidaSelecionada) {
     bebida = bebidaSelecionada.innerHTML;
 
     const elemento = document.querySelector(".bebida").querySelector(".borda-verde");
-    const check = document.querySelector(".bebida").querySelector("ion-icon");
     if (elemento !== null) {
         elemento.classList.remove("borda-verde")
         check.classList.add("escondido")
     } else {
     bebidaSelecionada.classList.add("borda-verde");
+    check = document.querySelector(".bebida").querySelector(".borda-verde").querySelector("ion-icon");
     check.classList.remove("escondido");
 }
 }
-
 
 
 function escolherSobremesa(sobremesaSelecionada) {
     sobremesa = sobremesaSelecionada.innerHTML;
 
     const elemento = document.querySelector(".sobremesa").querySelector(".borda-verde");
-    const checkButton = document.querySelector(".sobremesa").querySelector("ion-icon");
     if (elemento !== null) {
         elemento.classList.remove("borda-verde")
-        checkButton.classList.add("escondido")
+        check.classList.add("escondido")
     } else {
-    sobremesaSelecionada.classList.add("borda-verde");
-    checkButton.classList.remove("escondido"); }
+        sobremesaSelecionada.classList.add("borda-verde");
+    check = document.querySelector(".sobremesa").querySelector(".borda-verde").querySelector("ion-icon");
+    check.classList.remove("escondido");
 }
+}
+
 
 function pedir() {
 
@@ -64,9 +69,9 @@ function pedir() {
 function whats() {
     const text = "Fechar pedido";
     if(prato && bebida && sobremesa)  {
-    document.querySelector(".finalizar").innerHTML = text.link('https://wa.me/5519989879392?text=Ol%c3%a1%2c+gostaria+de+fazer+o+pedido%3a%0d%0a++++-+Prato%3a+Frango+Yin+Yang%0d%0a++++-+Bebida%3a+Coquinha+Gelada%0d%0a++++-+Sobremesa%3a+Pudim%0d%0a++++Total%3a+R%24+27.70'); }
-    else {
-        alert("Você deve marcar todas as opções!")
-    }
+    document.querySelector(".finalizar").innerHTML = text.link('https://wa.me/5519989879392?text=Ol%c3%a1%2c+gostaria+de+fazer+o+pedido%3a%0d%0a++++-+Prato%3a+Frango+Yin+Yang%0d%0a++++-+Bebida%3a+Coquinha+Gelada%0d%0a++++-+Sobremesa%3a+Pudim%0d%0a++++Total%3a+R%24+27.70'); 
+} else {
+    alert("Você deve marcar todas as opções!")
+}
 }
 
